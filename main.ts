6be44c69a92ca6cc81f8a3fdff6e8eeb4185cac8
MMM-tek <mtmirallesflores@gmail.com>
2026-03-08 15:39:09 +0100

    Actualizar main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,58 @@
 function Level () {
-    Levels = [tilemap`nivel1`, tilemap`nivel2`, tilemap`nivel5`]
-    Tipe = ["Tutorial", "Easy", "Medium"]
-    Modes = [0, 0, 1]
-    ays = [600, 600, 600]
-    vys = [-200, -200, -200]
-    vxs = [100, 100, -200]
-    if (Lvl <= Levels.length) {
+    Levels = [
+    tilemap`nivel1`,
+    tilemap`nivel2`,
+    tilemap`nivel5`,
+    tilemap`nivel3`,
+    tilemap`nivel6`
+    ]
+    Tipe = [
+    "Tutorial",
+    "Easy",
+    "Medium",
+    "What?",
+    "Medium"
+    ]
+    Modes = [
+    0,
+    0,
+    1,
+    3,
+    2
+    ]
+    ays = [
+    600,
+    600,
+    600,
+    400,
+    500
+    ]
+    vys = [
+    -200,
+    -200,
+    -200,
+    -200,
+    -15
+    ]
+    vxs = [
+    100,
+    100,
+    -200,
+    0,
+    100
+    ]
+    if (Lvl < Levels.length) {
         tiles.setCurrentTilemap(Levels[Lvl])
         tiles.placeOnRandomTile(mySprite, assets.tile`miMosaico8`)
         tiles.setTileAt(mySprite.tilemapLocation(), assets.tile`transparency16`)
         mySprite.ay = ays[Lvl]
         mySprite.vx = vxs[Lvl]
         game.splash("Level:" + Lvl, Tipe[Lvl])
+        if (Modes[Lvl] == 3) {
+            controller.moveSprite(mySprite, 100, 0)
+        } else {
+            controller.moveSprite(mySprite, 0, 0)
+        }
     } else {
         game.reset()
     }
@@ -54,6 +95,10 @@ game.onUpdate(function () {
         if (controller.anyButton.isPressed()) {
             mySprite.vy += vys[Lvl]
         }
+    } else if (Modes[Lvl] == 3) {
+        if ((controller.up.isPressed() || controller.A.isPressed()) && mySprite.isHittingTile(CollisionDirection.Bottom)) {
+            mySprite.vy += vys[Lvl]
+        }
     }
     if ((mySprite.isHittingTile(CollisionDirection.Bottom) || mySprite.isHittingTile(CollisionDirection.Top)) && (controller.anyButton.isPressed() || mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico1`))) {
         if (Modes[Lvl] == 0) {
@@ -62,7 +107,12 @@ game.onUpdate(function () {
             mySprite.ay = mySprite.ay * -1
         }
     }
-    if (mySprite.isHittingTile(CollisionDirection.Right) || mySprite.isHittingTile(CollisionDirection.Left) || (mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico10`) || mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico12`))) {
+    if (mySprite.isHittingTile(CollisionDirection.Right) || mySprite.isHittingTile(CollisionDirection.Left)) {
+        if (!(Modes[Lvl] == 3)) {
+            Level()
+        }
+    }
+    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico10`) || mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico12`)) {
         Level()
     }
     if (mySprite.tileKindAt(TileDirection.Center, assets.tile`miMosaico9`)) {
